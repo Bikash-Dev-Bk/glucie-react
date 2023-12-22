876c31d84d37312e956c16d3ce5a22ba282508f2
bikash-dev <bikash.dev.bk@gmail.com>
2023-12-22 18:10:18 +0600

    FAQ added

--- src/components/FAQs.jsx
+++ src/components/FAQs.jsx
@@ -1,11 +1,59 @@
-
-
 const FAQs = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="px-5 lg:px-64 py-10 lg:py-24" id="faqs">
+      <h2 className="text-3xl lg:text-5xl text-center font-semibold mb-8 lg:mb-20 md:px-12">
+        FAQs
+      </h2>
+      <div className="collapse collapse-plus border-t-2 mb-5 rounded-none">
+        <input type="radio" name="my-accordion-3" checked="checked" />
+        <div className="collapse-title text-xl font-medium">
+          Where is Glucie avaiable?
         </div>
-    );
+        <div className="collapse-content">
+          <p className="lg:w-[275px]">
+            “Sign up to receive your sensor, download the DecodedSelf app and
+            get real-time insights”
+          </p>
+        </div>
+      </div>
+      <div className="collapse collapse-plus border-t-2 mb-5 rounded-none">
+        <input type="radio" name="my-accordion-3" />
+        <div className="collapse-title text-xl font-medium">
+          Where is Glucie avaiable?
+        </div>
+        <div className="collapse-content">
+          <p className="lg:w-[275px]">
+            “Sign up to receive your sensor, download the DecodedSelf app and
+            get real-time insights”
+          </p>
+        </div>
+      </div>
+      <div className="collapse collapse-plus border-t-2 mb-5 rounded-none">
+        <input type="radio" name="my-accordion-3" />
+        <div className="collapse-title text-xl font-medium">
+          Where is Glucie avaiable?
+        </div>
+        <div className="collapse-content">
+          <p className="lg:w-[275px]">
+            “Sign up to receive your sensor, download the DecodedSelf app and
+            get real-time insights”
+          </p>
+        </div>
+      </div>
+      <div className="collapse collapse-plus border-t-2 mb-5 rounded-none">
+        <input type="radio" name="my-accordion-3" />
+        <div className="collapse-title text-xl font-medium">
+          Where is Glucie avaiable?
+        </div>
+        <div className="collapse-content">
+          <p className="lg:w-[275px]">
+            “Sign up to receive your sensor, download the DecodedSelf app and
+            get real-time insights”
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FAQs;
